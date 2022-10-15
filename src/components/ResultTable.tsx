51cff127ce7889a330b096db9de2d14cdc34bf06
Lester <32542078+lhes23@users.noreply.github.com>
2022-10-15 22:20:56 +0800

    add styles on ResultTable

--- src/components/ResultTable.tsx
+++ src/components/ResultTable.tsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { Reading } from "./Form"
+import { Reading } from "../App"
 
 const ResultTable = ({
   houseReadings,
@@ -10,79 +10,50 @@ const ResultTable = ({
 }) => {
   return (
     <>
-      {/* <table className="table-auto">
-        <thead>
-          <tr>
-            <th className="px-4 py-2">House</th>
-            <th className="px-4 py-2">Previous Reading</th>
-            <th className="px-4 py-2">Present Reading</th>
-            <th className="px-4 py-2">Consumption</th>
-            <th className="px-4 py-2">Peso Per Consumption</th>
-            <th className="px-4 py-2">Bill</th>
-          </tr>
-        </thead>
-        <tbody>
-          {houseReadings.map((house) => (
-            <tr>
-              <td className="border px-4 py-2">{house.name}</td>
-              <td className="border px-4 py-2">{house.previous}</td>
-              <td className="border px-4 py-2">{house.present}</td>
-              <td className="border px-4 py-2">{house.consumption}</td>
-              <td className="border px-4 py-2">₱ {pesoper}</td>
-              <td className="border px-4 py-2">₱ {house.bill}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table> */}
-
-      <div className="flex justify-center">
-        {houseReadings.map((house) => (
-          <>
-            <div className="flex flex-col items-center bg-white rounded-lg border shadow-md md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 mx-4">
-              <div className="flex flex-col justify-between p-4 leading-normal">
-                <h2>Due Date:</h2>
-                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                  {house.name}
-                </h5>
-                <div className="grid grid-cols-2">
-                  <div className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
-                    Previous Reading :{" "}
-                  </div>
-                  <div className="text-gray-700">{house.previous}</div>
-                </div>
-                <div className="grid grid-cols-2">
-                  <div className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
-                    Present Reading :{" "}
-                  </div>
-                  <div className="text-gray-700">{house.present}</div>
-                </div>
-                <div className="grid grid-cols-2">
-                  <div className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
-                    Consumption :{" "}
-                  </div>
-                  <div className="text-gray-700">{house.consumption}</div>
-                </div>
-                <div className="grid grid-cols-2">
-                  <div className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
-                    Peso per Consumption :{" "}
-                  </div>
-                  <div className="text-gray-700">₱ {pesoper}</div>
-                </div>
-                <div className="grid grid-cols-2">
-                  <div className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
-                    Total Bill :{" "}
-                  </div>
-                  <div className="text-gray-700 font-bold text-2xl">
-                    ₱ {house.bill}
-                  </div>
-                </div>
+      {houseReadings.map((house) => (
+        <div
+          key={house.name}
+          className="flex items-center bg-white rounded-lg border shadow-md w-full hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 mx-4"
+        >
+          <div className="flex flex-col justify-around p-4 leading-normal">
+            <h2>Due Date:</h2>
+            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+              {house.name}
+            </h5>
+            <div className={styles.divContainer}>
+              <div className={styles.divLabel}>Previous Reading : </div>
+              <div className={styles.divValue}>{house.previous}</div>
+            </div>
+            <div className={styles.divContainer}>
+              <div className={styles.divLabel}>Present Reading : </div>
+              <div className={styles.divValue}>{house.present}</div>
+            </div>
+            <div className={styles.divContainer}>
+              <div className={styles.divLabel}>Consumption : </div>
+              <div className={styles.divValue}>{house.consumption}</div>
+            </div>
+            <div className={styles.divContainer}>
+              <div className={styles.divLabel}>Peso per Consumption : </div>
+              <div className={styles.divValue}>₱ {pesoper}</div>
+            </div>
+            <div className={styles.divContainer}>
+              <div className={styles.divLabel}>Total Bill : </div>
+              <div className={`${styles.divValue} font-bold text-2xl`}>
+                ₱ {house.bill}
               </div>
             </div>
-          </>
-        ))}
-      </div>
+          </div>
+        </div>
+      ))}
     </>
   )
+}
+
+const styles = {
+  divContainer: "grid grid-cols-3 gap-2 sm:grid-cols-2",
+  divLabel:
+    "col-span-2 mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500",
+  divValue: "text-gray-700"
 }
 
 export default ResultTable
