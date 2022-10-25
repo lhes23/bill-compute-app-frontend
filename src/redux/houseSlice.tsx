93c5c0495c73e12f3addb6cb05696a72ddb8be71
Lester <32542078+lhes23@users.noreply.github.com>
2022-10-25 20:38:22 +0800

    remove Error: An immer producer returned a new value *and* modified its draft. Either return a new value *or* modify the draft

--- src/redux/houseSlice.tsx
+++ src/redux/houseSlice.tsx
@@ -9,14 +9,6 @@ interface IInitialState {
     consumption: number
     bill: number
   }[]
-  houseAValue: {
-    name: ""
-    tenant: ""
-    previous: 0
-    present: 0
-    consumption: 0
-    bill: 0
-  }
 }
 
 const initialState: IInitialState = {
@@ -29,28 +21,19 @@ const initialState: IInitialState = {
       consumption: 0,
       bill: 0
     }
-  ],
-  houseAValue: {
-    name: "",
-    tenant: "",
-    previous: 0,
-    present: 0,
-    consumption: 0,
-    bill: 0
-  }
+  ]
 }
 
 export const houseSlice = createSlice({
   name: "houses",
   initialState,
   reducers: {
-    setHouseAReadings: (state, action) => {
-      console.log({ action })
-      return (state.houseAValue = action.payload)
+    setHousesReadings: (state, action) => {
+      state.houses = action.payload
     }
   }
 })
 
-export const { setHouseAReadings } = houseSlice.actions
+export const { setHousesReadings } = houseSlice.actions
 
 export default houseSlice.reducer
