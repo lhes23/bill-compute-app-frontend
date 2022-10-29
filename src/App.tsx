41c4829c78b4aba1dcd0f8a263cff2e44d09a96e
Lester <32542078+lhes23@users.noreply.github.com>
2022-10-29 16:14:06 +0800

    install daisyui and add drawer

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,22 @@ import React from "react"
 import SideBar from "./layouts/SideBar"
 import Main from "./views/pages/Main"
 import Header from "./layouts/Header"
+import { GiHamburgerMenu } from "react-icons/gi"
 
 function App() {
   return (
     <>
-      <div className="flex h-screen">
-        <SideBar />
-        <div className="flex flex-col flex-1 w-full">
+      <div className="drawer">
+        <input id="my-drawer" type="checkbox" className="drawer-toggle" />
+        <div className="drawer-content">
+          {/* <!-- Page content here --> */}
+          <label htmlFor="my-drawer" className="btn btn-primary drawer-button">
+            <GiHamburgerMenu size={30} />
+          </label>
           <Header />
           <Main />
         </div>
+        <SideBar />
       </div>
     </>
   )
--- src/layouts/SideBar.tsx
+++ src/layouts/SideBar.tsx
@@ -5,10 +5,38 @@ import { GiHamburgerMenu } from "react-icons/gi"
 import { Link } from "react-router-dom"
 
 const SideBar = () => {
-  const [showSidebar, setShowSidebar] = useState<boolean>(true)
   return (
     <>
-      {showSidebar ? (
+      <div className="drawer-side">
+        <label htmlFor="my-drawer" className="drawer-overlay"></label>
+        <ul className="menu p-4 overflow-y-auto w-80 bg-base-100 text-base-content">
+          {/* <!-- Sidebar content here --> */}
+          <li className="relative px-3 py-2">
+            <span
+              className="absolute inset-y-0 left-0 w-1 bg-purple-600 rounded-tr-lg rounded-br-lg"
+              aria-hidden="true"
+            ></span>
+            <Link
+              className="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200 dark:text-gray-100"
+              to="/"
+            >
+              <AiOutlineDashboard size={25} />
+              <span className="ml-4">Dashboard</span>
+            </Link>
+          </li>
+          <li className="relative px-3 py-2">
+            <Link
+              className="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
+              to="/add-reading"
+            >
+              <FaWpforms size={25} />
+              <span className="ml-4">Forms</span>
+            </Link>
+          </li>
+        </ul>
+      </div>
+
+      {/* {showSidebar ? (
         <aside
           className={`top-0 left-0 w-[45vw] md:w-[20vw] bg-slate-200  p-10 text-purple-600 fixed h-screen z-40 transition duration-500 ease-in-out ${
             showSidebar ? "translate-x-0" : "translate-x-full"
@@ -53,7 +81,7 @@ const SideBar = () => {
             onClick={() => setShowSidebar(!showSidebar)}
           />
         </div>
-      )}
+      )} */}
     </>
   )
 }
