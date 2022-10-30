767aab38df0dd2b4f310fe99e71d7ba7717be6f1
Lester <32542078+lhes23@users.noreply.github.com>
2022-10-30 15:53:24 +0800

    connect back to front

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 import Main from "./views/pages/Main"
 import Header from "./layouts/Header"
 
 function App() {
   const [theme, setTheme] = useState<string>("halloween")
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch("http://127.0.0.1:8000/api/houses/")
+      const d = await res.json()
+      setData(d)
+    }
+    fetchData()
+  }, [])
+
+  console.log(data.map((d) => d))
   return (
     <>
       <div className="drawer" data-theme={theme}>
