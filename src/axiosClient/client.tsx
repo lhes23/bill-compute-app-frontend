7066fb244f71e197976bb355588c89c8e91a613c
Lester <32542078+lhes23@users.noreply.github.com>
2022-10-31 19:49:09 +0800

    fix connection

--- src/axiosClient/client.tsx
+++ src/axiosClient/client.tsx
@@ -2,6 +2,7 @@ import axios, { AxiosInstance } from "axios"
 
 const client: AxiosInstance = axios.create({
   baseURL: "http://192.168.100.30:8000/api/"
+  // baseURL: "http://localhost:8000/api/"
 })
 
 export default client
