f4c606328d12e4fdb76efb304ef0c09338a1c678
Lester <32542078+lhes23@users.noreply.github.com>
2022-12-09 19:18:03 +0800

    make use of the houseComponent

--- src/views/pages/HouseForm/components/HouseAComponentForm.tsx
+++ src/views/pages/HouseForm/components/HouseAComponentForm.tsx
@@ -25,41 +25,12 @@ const HouseAComponentForm = ({ tenantName }: { tenantName: string }) => {
 
   return (
     <>
-      <HouseComponent houseName={houseAData.name} tenantName={tenantName}>
-        <div className="relative">
-          <input
-            type="number"
-            id="previous_reading"
-            className="block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900 bg-transparent rounded-lg border-1 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-            placeholder="Previous Reading"
-            value={houseAData.previous}
-            onChange={previousChangeHandler}
-          />
-          <label
-            htmlFor="previous_reading"
-            className="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white dark:bg-gray-900 px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1"
-          >
-            Previous Reading
-          </label>
-        </div>
-
-        <div className="relative">
-          <input
-            type="number"
-            id="present_reading"
-            className="block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900 bg-transparent rounded-lg border-1 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-            placeholder="Present Reading"
-            value={houseAData.present}
-            onChange={presentChangeHandler}
-          />
-          <label
-            htmlFor="present_reading"
-            className="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white dark:bg-gray-900 px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1"
-          >
-            Present Reading
-          </label>
-        </div>
-      </HouseComponent>
+      <HouseComponent
+        house={houseAData}
+        tenantName={tenantName}
+        previousChangeHandler={previousChangeHandler}
+        presentChangeHandler={presentChangeHandler}
+      />
     </>
   )
 }
--- src/views/pages/HouseForm/components/HouseComponent.tsx
+++ src/views/pages/HouseForm/components/HouseComponent.tsx
@@ -1,15 +1,22 @@
+import { IHouseInitial } from "interfaces"
 import React from "react"
 
 interface IProps {
-  houseName: string
+  house: IHouseInitial
   tenantName: string
-  children: React.ReactNode
+  previousChangeHandler: (e: React.ChangeEvent<HTMLInputElement>) => {}
+  presentChangeHandler: (e: React.ChangeEvent<HTMLInputElement>) => {}
 }
 
-const HouseComponent = ({ houseName, tenantName, children }: IProps) => {
+const HouseComponent = ({
+  house,
+  tenantName,
+  previousChangeHandler,
+  presentChangeHandler
+}: IProps) => {
   return (
     <>
-      <h2 className="p-2">{houseName}</h2>
+      <h2 className="p-2">{house.name}</h2>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-4 my-4">
         <div className="relative">
           <h2 className="block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900 bg-transparent rounded-lg border-1 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer">
@@ -23,7 +30,41 @@ const HouseComponent = ({ houseName, tenantName, children }: IProps) => {
           </label>
         </div>
       </div>
-      <div className="grid grid-cols-2 gap-4">{children}</div>
+      <div className="grid grid-cols-2 gap-4">
+        <div className="relative">
+          <input
+            type="number"
+            id="previous_reading"
+            className="block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900 bg-transparent rounded-lg border-1 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+            placeholder="Previous Reading"
+            value={house.previous}
+            onChange={previousChangeHandler}
+          />
+          <label
+            htmlFor="previous_reading"
+            className="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white dark:bg-gray-900 px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1"
+          >
+            Previous Reading
+          </label>
+        </div>
+
+        <div className="relative">
+          <input
+            type="number"
+            id="present_reading"
+            className="block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900 bg-transparent rounded-lg border-1 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+            placeholder="Present Reading"
+            value={house.present}
+            onChange={presentChangeHandler}
+          />
+          <label
+            htmlFor="present_reading"
+            className="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white dark:bg-gray-900 px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1"
+          >
+            Present Reading
+          </label>
+        </div>
+      </div>
 
       <div className="sm:block" aria-hidden="true">
         <div className="py-5">
